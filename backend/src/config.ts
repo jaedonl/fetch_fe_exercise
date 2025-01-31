55cec39e415a93459ccea532a0b0f100c823483a
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2025-01-31 16:26:25 -0500

    -

--- backend/src/config.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const PORT = process.env.PORT;
-export const BASE_API_URL = process.env.BASE_API_URL;
--- backend/src/routes/locationRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getLocations, searchLocations } from "../controllers/locationController";
-
-const router = express.Router();
-
-router.post("/", getLocations);
-router.post("/search", searchLocations);
-
-export default router;
